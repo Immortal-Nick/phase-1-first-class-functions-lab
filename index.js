6e68abb38c9655edb07e50c25d423bd9c79dbbf4
Nixon <kiplangatnixon28@gmail.com>
2024-07-03 00:30:09 +0300

    creates first class functions

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+// Define the returnFirstTwoDrivers function
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+};
+
+// Define the returnLastTwoDrivers function
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+};
+
+// Create the selectingDrivers array containing the two functions
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const drivers = (['Antonia', 'Nuru', 'Amari', 'Mo']);
+console.log(selectingDrivers[0](drivers));
+console.log(selectingDrivers[1](drivers));
+
+function createFareMultiplier(multiplier) {
+    return function(fare) {
+        return fare * multiplier;
+    };
+}
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, selectFunction) {
+    return selectFunction(drivers);
+}
+console.log(selectDifferentDrivers(drivers, returnLastTwoDrivers));
+console.log(selectDifferentDrivers(drivers, returnFirstTwoDrivers));
+
